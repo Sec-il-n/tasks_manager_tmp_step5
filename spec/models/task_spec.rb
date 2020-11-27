abf4ed692e7e4a7970137f020ddeaf8ec13ec2cf
Sec-li-n <loveuallnbs0205@yahoo.com>
2020-11-27 17:01:36 +0900

    1回目提出修正+step2 SystemSpec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,14 +3,17 @@ describe 'タスクモデル機能', type: :model do
   describe '入力項目バリデーション' do
     context 'タスクのタイトルが空のとき' do
       it 'バリデーションに引っかかる' do
-        task = FactoryBot.create(:task_2, task_name: '', details: 'タスク名空 バリデーション')
-        expect(task).not_to be_valid
+        # task = Task.new(task_name: '', details: 'タスク名空 バリデーション')
+        task = FactoryBot.build(:task_2, task_name: '', details: 'タスク名空 バリデーション')
+        task.valid?
+        expect(task.errors.full_messages).to include('タスク名を入力してください')
       end
     end
     context 'タスクの詳細が空のとき' do
       it 'バリデーションに引っかかる' do
-        task = FactoryBot.create(:task_2, details:'')
-        expect(task).not_to be_valid
+        task = FactoryBot.build(:task_2, details:'')
+        task.valid?
+        expect(task.errors.full_messages).to include('詳細を入力してください')
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されているとき' do
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Task, type: :system do
       it '作成したタスクが画面に表示される' do
         visit new_task_path
         visit current_path
-        fill_in 'Task name', with: 'task_name'
-        fill_in 'Details', with: 'task_details'
+        fill_in 'タスク名', with: 'task_name'
+        fill_in '詳細', with: 'task_details'
         click_button '登録する'
         expect(page).to have_content('task_name')
         expect(page).to have_content('task_details')
@@ -18,6 +18,20 @@ RSpec.describe Task, type: :system do
         task = FactoryBot.create(:task, task_name: 'task上書き')
         visit tasks_path
         expect(page).to have_content('task上書き')
+      end
+    end
+    context '一覧画面表示されたとき' do
+      it '作成日時の降順で表示される' do
+        task = FactoryBot.create_list(:task_3, 2)
+        # task1 = FactoryBot.create(:task_3)
+        # task2 = FactoryBot.create(:task_3)
+        visit tasks_path
+        # 画面内の指定クラスの要素を配列で取得する
+        task_list = all('#sequence td')
+        expect(task_list[0].text).to have_content(task[1].task_name)
+        # expect(task_list[0]).to include(task[1])
+        # expect(task_list[0]).to have_text(task.task_name)
+        # expect(page).to have_content(task_list[1])
       end
     end
   end
