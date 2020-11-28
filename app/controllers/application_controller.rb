e4b46407821216f899df985669770e24e1cf75f8
Sec-li-n <loveuallnbs0205@yahoo.com>
2020-11-28 11:49:55 +0900

    Basic認証実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # 以下Concerns？？
   before_action :basic_auth
+  protect_from_forgery with: :exception
   private
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
