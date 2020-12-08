405269ea8998685cc265c452665cf725bae07b26
Sec-li-n <loveuallnbs0205@yahoo.com>
2020-12-08 09:09:19 +0900

    ユーザー登録から自動ログイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,22 @@
 class UsersController < ApplicationController
   before_action :set_user
+  skip_before_action :authenticate_user
   def new
     @user = User.new
   end
   def create
     @user = User.create(user_params)
     if @user.save
-      redirect_to user_path#＋ログイン状態にする
-      # redirect_to tasks_path
+      session[:user_id] = @user.id
+      # 出ない
+      redirect_to user_path(@user.id), notice: t('dictionary.words.user created')
     else
       # flash.now[:danger] = 'ユーザー登録に失敗しました。'
       render :new
     end
   end
   def show
-    # binding.pry
+
   end
   def edit
 
@@ -29,7 +31,7 @@ class UsersController < ApplicationController
   end
   private
   def user_params
-    params.require(:user).permit(:user_name, :email, :password)
+    params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
   end
   def set_user
     @user = User.find_by(id: params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :user_name, length: { maximum: 30 }
     validates :email, length: { maximum:255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
-    validates :password, length: {  minimum: 6 }, confirmation: true
-    validates :password_confirmation
+    validates :password, length: {  minimum: 6 }#, confirmation: true
+    # validates :password_confirmation
   end
   has_secure_password
   has_many :tasks
