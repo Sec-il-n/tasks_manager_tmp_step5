8b901f32b7a8eb2ed395db1bbec34e4c951ccf2d
Sec-li-n <loveuallnbs0205@yahoo.com>
2020-12-10 05:52:23 +0900

    管理者画面からのCRED機能とロールの追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,29 +1,60 @@
 class Admin::UsersController < ApplicationController
   before_action :admin_user, :set_user
   def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(params_admin_user)
+    if @user.save
+      redirect_to admin_user_path(@user.id), notice: t('admin.users.show.user add successed')
+    else
+      flash.now[:danger] = t('admin.users.form.user add failed')
+      render 'new'
+    end
   end
   def index
-    @users = User.all
+    # binding.pry
+    @users = User.includes(:tasks)
+    # @users = User.all
   end
   def show
-    binding.pry
     @tasks = @user.tasks
   end
   def edit
 
   end
   def update
+    # binding.pry
+      if @user.update(params_admin_user)
 
+        redirect_to admin_users_path, notice: t('admin.users.index.edited user')
+      else
+        render 'edit'
+      end
   end
   def destroy
-    @user.destroy
-    redirect_to admin_users_path, notice: t('dictionary.words.user deleted')
+    if @user.destroy
+      redirect_to admin_users_path, notice: t('dictionary.words.user deleted')
+    else
+      flash[:notice] = t('dictionary.words.admin needs minimum one')
+      # flash[:notice] = t('dictionary.words.admin needs minimum one')
+    end
   end
   private
   def admin_user
-    redirect_to tasks_path if current_user.admin = false
+    if current_user.admin == false
+      redirect_to tasks_path, notice: t('admin.users.index.need loggedin')
+    end
   end
   def set_user
     @user = User.find_by(id: params[:id])
+  end
+  def params_admin_user
+    params.require(:user).permit(:user_name, :email, :password, :password_confirmation, :admin)
+    # params.permit(:id, user: :admin)
+    # params.permit(:id, :_method, :authenticity_token, { user: :admin } )
+    # id, :_method, :authenticity_token => は許可しないとパラメタで受け取れないし、User.update(ここ)で使用すると「Userの属性ではない」と怒られる。　＝＞　StrongParametersは使えないのか？？
+    # binding.pry
+    # params.permit(:id, :user_name, :email, :password, :password_confirmation, :admin)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,18 @@ class User < ApplicationRecord
     # validates :password_confirmation
   end
   has_secure_password
+  before_destroy :cannot_destroy_last_admin
   has_many :tasks, dependent: :destroy
+
+  # scope :group_admin, -> { where(admin: true) }
+
+  private
+  def admin_exists?
+    User.where(admin: true).count == 1
+    # User.find_by(admin: true).count == 1
+    # User.find_by(admin: true).present?
+  end
+  def cannot_destroy_last_admin
+    throw(:abort) if admin_exists?
+  end
 end
